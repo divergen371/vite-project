6d0eb11679dd0971a47027b53228730893a972bb
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-25 06:08:35 +0000

    :alembic: オブジェクトの定義とUIの紐づけ

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+type typeOfFeatures = {
+  Sex: '男性' | '女性' | '性別は？'
+  Pclass: string
+  Age: number | '年齢は？'
+  Parch: number | '親・子の同伴者数は？'
+  SibSp: number | '兄弟姉妹の同伴者数は？'
+}
+
+const features = reactive<typeOfFeatures>({
+  Sex: '性別は？',
+  Pclass: '階級は？',
+  Age: '年齢は？',
+  Parch: '親・子の同伴者数は？',
+  SibSp: '兄弟姉妹の同伴者数は？',
+})
+</script>
 
 <template>
   <p class="bg-blue-200 text-2xl p-4 mt-4">Hello World</p>
   <div class="container mx-auto mt-4">
-    <select class="select select-primary mb-4">
+    <select v-model="features.Sex" class="select select-primary mb-4">
       <option disabled selected>性別は？</option>
       <option>男性</option>
       <option>女性</option>
     </select>
     <br />
-    <select class="select select-primary mb-4">
+    <select v-model="features.Pclass" class="select select-primary mb-4">
       <option disabled selected>階級は？</option>
       <option>上層クラス（お金持ち）</option>
       <option>中級クラス（一般階級）</option>
       <option>下層クラス（労働階級）</option>
     </select>
     <br />
-    <select class="select select-primary mb-4">
+    <select v-model="features.Age" class="select select-primary mb-4">
       <option disabled selected>年齢は？</option>
       // eslint-disable-next-line vue/require-v-for-key
       <option v-for="i in [...Array(121).keys()]">
@@ -25,7 +42,7 @@
     </select>
     歳
     <br />
-    <select class="select select-primary mb-4">
+    <select v-model="features.Parch" class="select select-primary mb-4">
       <option disabled selected>親・子の同伴者数は？</option>
       // eslint-disable-next-line vue/require-v-for-key
       <option v-for="i in [...Array(11).keys()]">
@@ -34,7 +51,7 @@
     </select>
     人
     <br />
-    <select class="select select-primary mb-4">
+    <select v-model="features.SibSp" class="select select-primary mb-4">
       <option disabled selected>兄弟姉妹の同伴者数は？</option>
       // eslint-disable-next-line vue/require-v-for-key
       <option v-for="i in [...Array(11).keys()]">{{ i }}</option>
